ed09130040951c7f9361db429dfca2ed40e1d39e
Shem <mongareshem1@gmail.com>
2026-08-19 15:42:35 +0300

    Define models and register the app in installed apps

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from tkinter.constants import CASCADE
 
-# Create your models here.
+from django.db import models
+from django.db.models.fields import TextField
+
+
+class Pizza(models.Model):
+    """The different kinds of pizzas."""
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        """Returns the name of the pizzas."""
+        return self.name
+
+
+class Topping(models.Model):
+    """Specific toppings used."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = TextField()
+
+    def __str__(self):
+        """Returns the specific toppings."""
+        return self.name
